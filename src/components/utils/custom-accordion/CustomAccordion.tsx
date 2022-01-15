a3091c074d687defc4cf0ec9a65867adc8767530
Hiraj Dey <deyhiraj@gmail.com>
2022-01-16 01:09:07 +0530

    feat:customAccordion inprogress

---
+++ src/components/utils/custom-accordion/CustomAccordion.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Styles from './CustomAccordion.module.scss';
+
+
+interface CustomAccordionProps{
+    
+}
+
+const CustomAccordion = ({}:CustomAccordionProps) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CustomAccordion
+
+
